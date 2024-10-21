0514b77463514a835e5b448bd700b40df4dafa12
dinisroxo99 <80784809+dinisroxo99@users.noreply.github.com>
2024-10-21 13:19:11 +0100

    Update WarehouseDTO.java
    Only for Getters

--- wedelivery/src/main/java/pt/ipleiria/estg/dei/ei/dae/wedelivery/dtos/WarehouseDTO.java
+++ wedelivery/src/main/java/pt/ipleiria/estg/dei/ei/dae/wedelivery/dtos/WarehouseDTO.java
@@ -1,4 +1,44 @@
 package pt.ipleiria.estg.dei.ei.dae.wedelivery.dtos;
 
-public class WarehouseDTO {
+import jakarta.validation.constraints.NotNull;
+import pt.ipleiria.estg.dei.ei.dae.wedelivery.entities.Warehouse;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WarehouseDTO implements Serializable {
+    private String name;
+    private String address;
+    private String city;
+    private String postalCode;
+
+    public WarehouseDTO() {
+    }
+    public WarehouseDTO(String name, String address, String city, String postalCode) {
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.postalCode = postalCode;
+    }
+
+    public String getName() {return name;}
+    public String getAddress() {return address;}
+    public String getCity() {return city;}
+    public String getPostalCode() {return postalCode;}
+
+
+    public static WarehouseDTO from(Warehouse warehouse) {
+        return new WarehouseDTO(
+                warehouse.getName(),
+                warehouse.getAddress(),
+                warehouse.getCity(),
+                warehouse.getPostalCode()
+        );
+    }
+    public static List<WarehouseDTO> from(List<Warehouse> warehouses) {
+        return warehouses.stream().map(WarehouseDTO::from).collect(Collectors.toList());
+    }
+
+
 }
