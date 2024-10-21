dad8f90fd2172cf050ea3856dfb24dd3d001f41e
dinisroxo99 <80784809+dinisroxo99@users.noreply.github.com>
2024-10-21 13:15:57 +0100

    Update productBean.java

--- wedelivery/src/main/java/pt/ipleiria/estg/dei/ei/dae/wedelivery/ejbs/ProductBean.java
+++ wedelivery/src/main/java/pt/ipleiria/estg/dei/ei/dae/wedelivery/ejbs/ProductBean.java
@@ -1,4 +1,69 @@
 package pt.ipleiria.estg.dei.ei.dae.wedelivery.ejbs;
 
+import jakarta.ejb.EJB;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.hibernate.Hibernate;
+import pt.ipleiria.estg.dei.ei.dae.wedelivery.entities.Client;
+import pt.ipleiria.estg.dei.ei.dae.wedelivery.entities.Product;
+
+import java.util.List;
+
+@Stateless
 public class ProductBean {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @EJB
+    private WarehouseBean warehouseBean;
+    //(long id, String name, String description, double price, String image, int quantity,boolean available, boolean haveSensor)
+    public void create(long id, String name, String description, double price, String image, int quantity, boolean available, boolean haveSensor) {
+        var product = new Product(id, name, description, price, image, quantity, available, haveSensor);
+        entityManager.persist(product);
+    }
+
+    public List<Product> findAll() {
+        // remember, maps to: “SELECT s FROM product s ORDER BY s.name”
+        var products = entityManager.createNamedQuery("getAllProducts", Product.class).getResultList();
+        products.forEach(product -> Hibernate.initialize(product.getWarehouse()));
+        return products;
+    }
+
+    public Product findById(long id) {
+        var product = entityManager.find(Product.class, id);
+        if (product == null) {
+            throw new RuntimeException("product " + id + " not found");
+        }
+        return product;
+    }
+
+    public List<Product> findByName(String name) {
+        var products = entityManager.find(Product.class, name);
+        if (products == null) {
+            throw new RuntimeException("product " + name + " not found");
+        }
+        return List.of(products);
+    }
+
+
+
+
+    public void addProductToWarehouse(String name, long productId) {
+        var warehouse = warehouseBean.find(name);
+        var product = findById(productId);
+
+        if(!warehouse.getProducts().contains(product)){
+            warehouse.addProduct(product);
+            product.addWarehouse(warehouse);
+            entityManager.merge(warehouse);
+            entityManager.merge(product);
+        }
+    }
+    public Product findInWarehouse(long id){
+        var product = this.findById(id);
+        Hibernate.initialize(product.getWarehouse());
+        return product;
+    }
+
 }
