192c636259da10a4626436c7945d6fb7636c958c
dinisroxo99 <80784809+dinisroxo99@users.noreply.github.com>
2024-10-21 13:16:34 +0100

    Update WarehouseBean.java

--- wedelivery/src/main/java/pt/ipleiria/estg/dei/ei/dae/wedelivery/ejbs/WarehouseBean.java
+++ wedelivery/src/main/java/pt/ipleiria/estg/dei/ei/dae/wedelivery/ejbs/WarehouseBean.java
@@ -1,4 +1,44 @@
 package pt.ipleiria.estg.dei.ei.dae.wedelivery.ejbs;
+import jakarta.ejb.EJB;
+import jakarta.ejb.EJBException;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import pt.ipleiria.estg.dei.ei.dae.wedelivery.entities.Product;
+import pt.ipleiria.estg.dei.ei.dae.wedelivery.entities.Warehouse;
 
+import java.util.List;
+
+
+@Stateless
 public class WarehouseBean {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    //String name, String address, String city, String postalCode
+    public void create(String name, String address, String city, String postalCode) {
+        var warehouse = new Warehouse(name, address, city, postalCode);
+        entityManager.persist(warehouse);
+    }
+
+    public List<Warehouse> findAll() {
+        // remember, maps to: “SELECT s FROM warehouse s ORDER BY s.name”
+        return entityManager.createNamedQuery("getAllWarehouses", Warehouse.class).getResultList();
+    }
+
+    public List<Warehouse> findByName(String name) {
+        return entityManager.createNamedQuery("getWarehouseByName", Warehouse.class).setParameter("name", name).getResultList();
+    }
+
+    public Warehouse find(String name) {
+        var warehouse = entityManager.find(Warehouse.class, name);
+        if (warehouse == null) {
+            throw new EJBException("Warehouse " + name + " not found");
+        }
+        return warehouse;
+    }
+
+
+
+
 }
