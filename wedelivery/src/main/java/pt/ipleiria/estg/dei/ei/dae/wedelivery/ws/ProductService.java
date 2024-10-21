80a9bfc4854f15bbebc284f2f73a99e27a7a8365
dinisroxo99 <80784809+dinisroxo99@users.noreply.github.com>
2024-10-21 13:20:34 +0100

    Update ProductServices
    routes:
    /products
    /products/{id}

--- wedelivery/src/main/java/pt/ipleiria/estg/dei/ei/dae/wedelivery/ws/ProductService.java
+++ wedelivery/src/main/java/pt/ipleiria/estg/dei/ei/dae/wedelivery/ws/ProductService.java
@@ -1,4 +1,43 @@
 package pt.ipleiria.estg.dei.ei.dae.wedelivery.ws;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Response;
+import jakarta.ejb.EJB;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import pt.ipleiria.estg.dei.ei.dae.wedelivery.dtos.ProductDTO;
+import pt.ipleiria.estg.dei.ei.dae.wedelivery.dtos.WarehouseDTO;
+import pt.ipleiria.estg.dei.ei.dae.wedelivery.ejbs.ProductBean;
+import pt.ipleiria.estg.dei.ei.dae.wedelivery.ejbs.WarehouseBean;
+import pt.ipleiria.estg.dei.ei.dae.wedelivery.entities.Product;
 
+import java.util.List;
+
+@Path("/products")
+@Produces({MediaType.APPLICATION_JSON}) // injects header “Content-Type: application/json”
+@Consumes({MediaType.APPLICATION_JSON})
 public class ProductService {
+    @EJB
+    private ProductBean productBean;
+
+    @EJB
+    private WarehouseBean warehouseBean;
+
+    @GET
+    @Path("/")
+    public Response getAllProducts() {
+        List<ProductDTO> productDTOs = ProductDTO.from(productBean.findAll());
+        return Response.ok(productDTOs).build();
+    }
+
+    @GET
+    @Path("{id}")
+    public Response getProductById(@PathParam("id") long id) {
+        Product product = productBean.findById(id);
+        ProductDTO productDTO = ProductDTO.from(product);
+        return Response.ok(productDTO).build();
+    }
+
+
 }
