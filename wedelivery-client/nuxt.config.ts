817ad30f6d53ef2df46ca76d7135428920eec105
dinisroxo99 <80784809+dinisroxo99@users.noreply.github.com>
2024-10-22 10:22:24 +0100

    Update nuxt.config.ts for layout default

--- wedelivery-client/nuxt.config.ts
+++ wedelivery-client/nuxt.config.ts
@@ -1,13 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
+
   css: [
-    'bootstrap/dist/css/bootstrap.min.css'
+    'bootstrap/dist/css/bootstrap.css',
   ],
+  plugins: [],
+
+
   devtools: { enabled: true },
+
   runtimeConfig: {
     public: {
       API_URL: process.env.API_URL || "http://localhost:8080/wedelivery/api",
+      URL: process.env.URL || "http://localhost:3000",
     },
   },
 });
