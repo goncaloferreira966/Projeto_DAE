4a893fe0b2d303f6cf9eb36bdbbb0ec60072cd7a
GuilhermeCruzPT <guilhermecruz128@gmail.com>
2024-10-22 16:30:15 +0100

    EJBS - VolumeBean

--- wedelivery/src/main/java/pt/ipleiria/estg/dei/ei/dae/wedelivery/ejbs/VolumeBean.java
+++ wedelivery/src/main/java/pt/ipleiria/estg/dei/ei/dae/wedelivery/ejbs/VolumeBean.java
@@ -1,4 +1,32 @@
 package pt.ipleiria.estg.dei.ei.dae.wedelivery.ejbs;
 
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import pt.ipleiria.estg.dei.ei.dae.wedelivery.entities.Volume;
+
+import java.util.List;
+
+@Stateless
 public class VolumeBean {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public void create(long id) {
+        var volume = new Volume(id);
+        entityManager.persist(volume);
+    }
+
+    public List<Volume> findAll() {
+        // remember, maps to: “SELECT s FROM volume v ORDER BY v.id”
+        return entityManager.createNamedQuery("getAllVolumes", Volume.class).getResultList();
+    }
+
+    public Volume find(long id) {
+        var volume = entityManager.find(Volume.class, id);
+        if (volume == null) {
+            throw new RuntimeException("volume " + id + " not found");
+        }
+        return volume;
+    }
 }
