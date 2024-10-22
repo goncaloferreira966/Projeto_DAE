78e9633c7419d9f7a14c5f2258dad5b5a9c8b854
dinisroxo99 <80784809+dinisroxo99@users.noreply.github.com>
2024-10-22 10:23:13 +0100

    apply layout default

--- wedelivery-client/app.vue
+++ wedelivery-client/app.vue
@@ -1,3 +1,23 @@
+<!-- app.vue -->
 <template>
-  <NuxtPage />
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
+
+<script setup>
+import 'bootstrap/dist/css/bootstrap.min.css'; // CSS pode ser carregado em qualquer lado
+
+// Importar o JavaScript do Bootstrap apenas no lado do cliente
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  import('bootstrap/dist/js/bootstrap.bundle.min.js');
+});
+</script>
+
+<style scoped>
+/* Global styles can go here */
+</style>
