54ac30ca74cad2c6bf6291bef291087d287feebd
GuilhermeCruzPT <guilhermecruz128@gmail.com>
2024-10-22 15:19:04 +0100

    Entities - Class Restriction

--- wedelivery/src/main/java/pt/ipleiria/estg/dei/ei/dae/wedelivery/entities/Restriction.java
+++ wedelivery/src/main/java/pt/ipleiria/estg/dei/ei/dae/wedelivery/entities/Restriction.java
@@ -1,4 +1,57 @@
 package pt.ipleiria.estg.dei.ei.dae.wedelivery.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
 public class Restriction {
+    @Id
+    private long id;
+    @NotNull
+    private String type;
+    private int maxValue;
+    private int minValue;
+
+    public Restriction() {
+    }
+
+    public Restriction(long id, String type, int maxValue, int minValue) {
+        this.id = id;
+        this.type = type;
+        this.maxValue = maxValue;
+        this.minValue = minValue;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getMaxValue() {
+        return maxValue;
+    }
+
+    public int getMinValue() {
+        return minValue;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setMaxValue(int maxValue) {
+        this.maxValue = maxValue;
+    }
+
+    public void setMinValue(int minValue) {
+        this.minValue = minValue;
+    }
 }
