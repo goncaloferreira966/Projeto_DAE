85aec560842d0e551d9b0747fef53fe72c28e273
dinisroxo99 <80784809+dinisroxo99@users.noreply.github.com>
2024-10-22 10:33:26 +0100

    Create newPageExample.vue

---
+++ wedelivery-client/pages/newPageExample.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <h1 class="text-center my-5">New Page Example</h1>
+        <p class="text-center">This is a new page example.</p>
+    </div>
+</template>
+<script setup>
+definePageMeta({
+  layout: 'default' // Garante que o layout padrão seja usado
+});
+</script>
+<style scoped>
+/* Global styles can go here */
+</style>
