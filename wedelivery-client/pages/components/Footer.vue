a0d3145f0cd48e0a8998cc89d9ca9b47f4ed3003
dinisroxo99 <80784809+dinisroxo99@users.noreply.github.com>
2024-10-22 10:20:31 +0100

    create tag Footer and NabBar

---
+++ wedelivery-client/pages/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer class="mt-5 bg-light text-center py-4">
+    <p class="mb-0">© {{ new Date().getFullYear() }} WeDelivery. All rights reserved.</p>
+    <p>
+      <nuxt-link  class="text-secondary">Privacy Policy</nuxt-link> |
+      <nuxt-link class="text-secondary">Contact Us</nuxt-link>
+    </p>
+  </footer>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+
+</style>
---
+++ wedelivery-client/pages/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">WeDelivery</a>
+        <button 
+          class="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarNav" 
+          aria-controls="navbarNav" 
+          aria-expanded="false" 
+          aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <nuxt-link class="nav-link" to="/">Home</nuxt-link>
+            </li>
+            <li class="nav-item">
+              <nuxt-link class="nav-link" to="/manager">Orders</nuxt-link>
+            </li>
+            <li class="nav-item">
+              <nuxt-link class="nav-link" to="/e-commerce">E-Commerce</nuxt-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </template>
+  
+  <script setup>
+  </script>
+  <style scoped>
+  /* Estilos específicos para a navbar, se necessário */
+  </style>
