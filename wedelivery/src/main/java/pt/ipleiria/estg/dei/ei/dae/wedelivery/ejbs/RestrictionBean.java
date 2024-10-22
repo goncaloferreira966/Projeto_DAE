4656efd4148395999ee718ab60b95e02a45c300a
GuilhermeCruzPT <guilhermecruz128@gmail.com>
2024-10-22 15:24:59 +0100

    EJBS - RestrictionBean

--- wedelivery/src/main/java/pt/ipleiria/estg/dei/ei/dae/wedelivery/ejbs/RestrictionBean.java
+++ wedelivery/src/main/java/pt/ipleiria/estg/dei/ei/dae/wedelivery/ejbs/RestrictionBean.java
@@ -1,4 +1,32 @@
 package pt.ipleiria.estg.dei.ei.dae.wedelivery.ejbs;
 
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import pt.ipleiria.estg.dei.ei.dae.wedelivery.entities.Restriction;
+
+import java.util.List;
+
+@Stateless
 public class RestrictionBean {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public void create(long id, String type, int maxValue, int minValue) {
+        var restriction = new Restriction(id, type, maxValue, minValue);
+        entityManager.persist(restriction);
+    }
+
+    public List<Restriction> findAll() {
+        // remember, maps to: “SELECT s FROM restriction r ORDER BY r.id”
+        return entityManager.createNamedQuery("getAllRestrictions", Restriction.class).getResultList();
+    }
+
+    public Restriction find(long id) {
+        var restriction = entityManager.find(Restriction.class, id);
+        if (restriction == null) {
+            throw new RuntimeException("restriction " + id + " not found");
+        }
+        return restriction;
+    }
 }
