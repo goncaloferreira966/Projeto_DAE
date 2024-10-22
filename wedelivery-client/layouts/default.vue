632962f0354b9721aeb7a584cfed6e8abc24ef5c
dinisroxo99 <80784809+dinisroxo99@users.noreply.github.com>
2024-10-22 10:22:47 +0100

    create layout default.vue

---
+++ wedelivery-client/layouts/default.vue
@@ -0,0 +1,22 @@
+<!-- layouts/default.vue -->
+<template>
+  <div>
+    <Navbar />
+    <main>
+      <slot></slot> <!-- O conteúdo da página será renderizado aqui -->
+    </main>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script setup>
+import Navbar from '../pages/components/Navbar.vue'; // Ajuste se necessário
+import Footer from '../pages/components/Footer.vue'; // Ajuste se necessário
+</script>
+
+<style scoped>
+main {
+  padding: 20px; /* Espaçamento para o conteúdo */
+  min-height: calc(100vh - 100px); /* Ajuste a altura mínima conforme necessário */
+}
+</style>
