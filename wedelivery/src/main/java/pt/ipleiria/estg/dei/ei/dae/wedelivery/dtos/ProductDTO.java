9439099a9bf00db3e1c6388d8a494c3b359a8d90
dinisroxo99 <80784809+dinisroxo99@users.noreply.github.com>
2024-10-23 17:44:06 +0100

    add setters and optimize constructer

--- wedelivery/src/main/java/pt/ipleiria/estg/dei/ei/dae/wedelivery/dtos/ProductDTO.java
+++ wedelivery/src/main/java/pt/ipleiria/estg/dei/ei/dae/wedelivery/dtos/ProductDTO.java
@@ -2,6 +2,8 @@ package pt.ipleiria.estg.dei.ei.dae.wedelivery.dtos;
 
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
+import pt.ipleiria.estg.dei.ei.dae.wedelivery.ejbs.SupplierBean;
+import pt.ipleiria.estg.dei.ei.dae.wedelivery.ejbs.WarehouseBean;
 import pt.ipleiria.estg.dei.ei.dae.wedelivery.entities.Product;
 
 import java.io.Serializable;
@@ -18,12 +20,14 @@ public class ProductDTO implements Serializable {
     private int quantity;
     private boolean available;
     private boolean haveSensor = false;
-    private String warehouseName;
+    private WarehouseDTO warehouse;
+
+    private SupplierDTO supplier;
 
     public ProductDTO() {
     }
 
-    public ProductDTO(long id, String name, String description, double price, String image, int quantity, boolean available, boolean haveSensor, String warehouseName) {
+    public ProductDTO(long id, String name, String description, double price, String image, int quantity, boolean available, boolean haveSensor) {
         this.id = id;
         this.name = name;
         this.description = description;
@@ -32,27 +36,31 @@ public class ProductDTO implements Serializable {
         this.quantity = quantity;
         this.available = available;
         this.haveSensor = haveSensor;
-        this.warehouseName = warehouseName;
+        this.warehouse = new WarehouseDTO();
+        this.supplier = new SupplierDTO();
     }
 
     public long getId() {return id;}
-
     public String getName() {return name;}
-
     public String getDescription() {return description;}
-
     public double getPrice() {return price;}
-
     public String getImage() {return image;}
-
     public int getQuantity() {return quantity;}
-
     public boolean isAvailable() {return available;}
-
     public boolean getHaveSensor() {return haveSensor;}
+    public WarehouseDTO getWarehouse() {return warehouse;}
+    public SupplierDTO getSupplier() {return supplier;}
 
-    public String getWarehouseName() {return warehouseName;}
-
+    public void setId(long id) {this.id = id;}
+    public void setName(String name) {this.name = name;}
+    public void setDescription(String description) {this.description = description;}
+    public void setPrice(double price) {this.price = price;}
+    public void setImage(String image) {this.image = image;}
+    public void setQuantity(int quantity) {this.quantity = quantity;}
+    public void setAvailable(boolean available) {this.available = available;}
+    public void setHaveSensor(boolean haveSensor) {this.haveSensor = haveSensor;}
+    public void setWarehouse(WarehouseDTO warehouse) {this.warehouse = warehouse;}
+    public void setSupplier(SupplierDTO supplier) {this.supplier = supplier;}
 
     public static ProductDTO from(Product product) {
         return new ProductDTO(
@@ -63,8 +71,7 @@ public class ProductDTO implements Serializable {
                 product.getImage(),
                 product.getQuantity(),
                 product.isAvailable(),
-                product.getHaveSensor(),
-                product.getWarehouse().getName()
+                product.getHaveSensor()
         );
     }
 
